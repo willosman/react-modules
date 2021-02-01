23c5877903c4db873e1070435b6636c8cbcc95fd
William Osman <wosman007@gmail.com>
2021-01-31 21:56:28 -0500

    commit 1.31.21

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -3,6 +3,11 @@ import {Route, Link, Switch} from 'react-router-dom'
 import Home from './Home'
 import Resources from './Resources'
 import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo';
+import JSXRules from '../concepts/JSXRules';
+import State from '../concepts/State';
+import Effects from '../concepts/Effects';
+import PropsDemo from '../concepts/PropsDemo';
+import Hooks from '../concepts/Hooks';
 
 const Sidebar = () => {
     return (
@@ -11,6 +16,11 @@ const Sidebar = () => {
                 <ul className="sidebar-list list-unstyled">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/functionalcomponent">Functional Component</Link></li>
+                    <li><Link to ="/jsxrules">JSX Rules</Link></li>
+                    <li><Link to ="/state">useState</Link></li>
+                    <li><Link to ="/effects">useEffect</Link></li>
+                    <li><Link to ="/propsdemo">Props Demo</Link></li>
+                    <li><Link to ="/hooks">Hooks</Link></li>
                     <li><Link to="/resources">Resources</Link></li>
                 </ul>
             </div>
@@ -19,7 +29,12 @@ const Sidebar = () => {
                     <Route exact path="/home"><Home /></Route>
                     <Route exact path="/resources"><Resources /></Route>
                     <Route exact path="/"><Home /></Route>
+                    <Route exact path ="/jsxrules"><JSXRules /></Route>
                     <Route exact path="/functionalcomponent"><FunctionalComponentDemo/></Route>
+                    <Route exact path="/state"><State /></Route>
+                    <Route exact path="/effects"><Effects /></Route>
+                    <Route exact path="/propsdemo"><PropsDemo/></Route>
+                    <Route exact path="/hooks"><Hooks/></Route>
                 </Switch>
             </div>
         </div>
